aaeefa7bd99eecc5366336370afc56dbe322c6b7
Carley Tripp <carley.tripp@outlook.com>
2019-02-09 23:51:45 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209234936) do
+ActiveRecord::Schema.define(version: 20190209235127) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.string   "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "bio"
   end
 
   create_table "categories", force: :cascade do |t|
